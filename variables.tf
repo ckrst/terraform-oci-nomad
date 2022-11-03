050ede2f55c37faf2e9474431e2186c23941ece4
Vinicius Kirst <vinicius.kirst@gmail.com>
2022-11-03 02:27:58 -0300

    ports as set of strings

--- variables.tf
+++ variables.tf
@@ -31,11 +31,11 @@ variable "api_allowed_ip" {
 }
 
 variable "allow_tcp_private_ports" {
-  type        = list(number)
+  type        = set(string)
   description = "Allow tcp ports private"
 }
 
 variable "allow_tcp_public_ports" {
-  type        = list(number)
+  type        = set(string)
   description = "Allow tcp ports public"
 }
